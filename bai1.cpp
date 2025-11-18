b156f9baaf19629d28e742599f84fd5d5dea9638
CuongVo24 <cmax.24042006@gmail.com>
2025-11-18 17:45:51 +0700

    Update bai1.cpp

--- bai1.cpp
+++ bai1.cpp
@@ -2,50 +2,44 @@
 
 using namespace std;
 
-#define NOE 10 // [cite: 100]
+#define NOE 10
 
 class A {
 private:
-    int info[NOE]; // [cite: 103]
+    int info[NOE];
 
 public:
-    // Định nghĩa kiểu indexOf là con trỏ int để có thể trả về tham chiếu/địa chỉ
-    typedef int* indexOf; // [cite: 107]
+    typedef int* indexOf;
 
     A() {
         for (int i = 0; i < NOE; ++i) {
-            info[i] = i + NOE; // [cite: 119]
+            info[i] = i + NOE;
         }
     }
 
-    // Trả về địa chỉ của phần tử thứ i
     indexOf GetInfo(const int& i) {
-        // Logic: info là mảng, info + i là địa chỉ phần tử thứ i
-        return info + i; // [cite: 128]
+        return info + i;
     }
 
     void Xuat(ostream& os) {
         for (int i = 0; i < NOE; ++i) {
-            os << info[i] << ", "; // [cite: 141]
+            os << info[i] << ", ";
         }
         os << endl;
     }
 };
 
 int main() {
-    const int i = 7; // [cite: 147]
+    const int i = 7;
     A a;
     
-    // Sử dụng kiểu dữ liệu nội bộ A::indexOf
-    A::indexOf io = a.GetInfo(i); // [cite: 151]
+    A::indexOf io = a.GetInfo(i);
     
-    // Dereference con trỏ để lấy giá trị
     cout << *io << endl; // 
     
-    // Thay đổi giá trị thông qua con trỏ (minh họa việc truy cập trực tiếp vùng nhớ)
-    *io = 10; // [cite: 154]
+    *io = 10;
     
-    a.Xuat(cout); // [cite: 155]
+    a.Xuat(cout);
     
     system("pause");
     return 0;
